2932c61090452ff10eed5a13d57edff1505e7c1f
Simply92 <pedrouner92@gmail.com>
2024-05-08 17:41:40 -0300

    empezando con test de productos

---
+++ src/handlers/__tests__/product.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import server from '../../server'
+
+describe('POST /api/products', () => {
+    it('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+})
